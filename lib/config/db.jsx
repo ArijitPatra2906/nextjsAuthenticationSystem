d1fdb5b9f9c0a35311540d36417a56c87b0dd64c
ArijitPatra2906 <patraarijit440@gmail.com>
2023-12-14 21:43:28 +0530

    mongo url added in env

--- lib/config/db.jsx
+++ lib/config/db.jsx
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const ConnectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://arijit:arijit@cluster0.llqcyfx.mongodb.net/?retryWrites=true&w=majority"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log(`the db is connect with ${mongoose.connection.host}`);
   } catch (error) {
     mongoose.disconnect();
